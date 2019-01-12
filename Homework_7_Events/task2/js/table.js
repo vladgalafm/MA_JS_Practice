a911f7e8e583c790f8ca9910ce21cbb05f868ea1
Vladyslav Halushkin <v.halushkin@gmail.com>
2019-01-12 12:37:17 +0200

    table upd

--- Homework_7_Events/task2/js/table.js
+++ Homework_7_Events/task2/js/table.js
@@ -53,22 +53,90 @@
         rows[i].setAttribute('data-page', Math.ceil(++count / rowsPerPageInput) + '');
       }
       pagesAmount = Math.ceil(count / rowsPerPageInput);
-      if (rows[i].dataset.filter && rows[i].hasAttribute('data-page')) {
-        rows[i].classList.remove('display-none');
+      showCurrentPage(i);
+    }
+
+    pagerControlsUpdate();
+  }
+
+  function showCurrentPage(i) {
+    if (rows[i].dataset.filter && rows[i].getAttribute('data-page') === currentPage) {
+      rows[i].classList.remove('display-none');
+    } else {
+      rows[i].classList.add('display-none');
+    }
+  }
+
+  function createNewPageLink(i) {
+    var newLi = document.createElement('li');
+    newLi.className = 'pager__list-item';
+    newLi.setAttribute('data-go-to-page', i);
+    if (i === 1 || i === pagesAmount) {
+      pagerList.appendChild(newLi);
+    } else {
+      if (pagerList.querySelector('[data-go-to-page="' + (i - 1) + '"]') !== null) {
+        insertAfter(newLi, pagerList.querySelector('[data-go-to-page="' + (i - 1) + '"]'));
       } else {
-        rows[i].classList.add('display-none');
+        insertAfter(newLi, pagerList.querySelector('[data-go-to-page="1"]'));
       }
-      /*
-      if (i < rowsPerPageInput) {
-        rows[i].classList.remove('display-none');
-      } else if (i + 1 > rowsPerPageInput && !rows[i].classList.contains('display-none')) {
-        rows[i].classList.add('display-none');
+    }
+
+    var newLink = document.createElement('a');
+    newLink.className = 'pager__page';
+    newLink.innerHTML = i;
+    newLi.appendChild(newLink);
+
+    if (+newLink.innerHTML === currentPage) {
+      newLink.classList.add('pager__page--current');
+    }
+  }
+
+  function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
+
+  function pageLinksMappingCheckout() {
+    var linksList = pagerList.querySelectorAll('li');
+    for (var i = 0; i < linksList.length; i++) {
+      var currentLinkInner = linksList[i].querySelector('a').innerHTML;
+      var nextLinkInner = linksList[i + 1].querySelector('a').innerHTML;
+      if (+nextLinkInner - +currentLinkInner === 2) {
+        createNewPageLink(+currentLinkInner + 1);
+      } else if (+nextLinkInner - +currentLinkInner > 2) {
+        var newLi = document.createElement('li');
+        newLi.className = 'pager__list-item';
+        newLi.innerHTML = '...';
+        pagerList.insertBefore(newLi, linksList[i + 1]);
       }
-      */
     }
   }
 
   function pagerControlsUpdate() {
+    if (pagesAmount < 6) {
+      for (var i = 1; i < pagesAmount + 1; i++) {
+        createNewPageLink(i);
+      }
+    } else {
+      if (currentPage === 1 || currentPage === 2) {
+        for (i = 1; i < 4; i++) {
+          createNewPageLink(i);
+        }
+        createNewPageLink(pagesAmount);
+      } else if (currentPage === pagesAmount || currentPage === pagesAmount - 1) {
+        createNewPageLink(1);
+        for (i = pagesAmount - 2; i < pagesAmount + 1; i++) {
+          createNewPageLink(i);
+        }
+      } else {
+        createNewPageLink(1);
+        for (i = currentPage - 1; i < currentPage + 2; i++) {
+          createNewPageLink(i);
+        }
+        createNewPageLink(pagesAmount);
+      }
+    }
+
+    pageLinksMappingCheckout();
 
     if (currentPage === 1) {
       pagerPrev.disabled = true;
