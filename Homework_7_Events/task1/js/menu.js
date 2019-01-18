01c282d7bca6ea45f44c97d4bf601c3a3679dd68
Vladyslav Halushkin <v.halushkin@gmail.com>
2019-01-18 19:53:32 +0200

    hw7 menu width calculation fixed

--- Homework_7_Events/task1/js/menu.js
+++ Homework_7_Events/task1/js/menu.js
@@ -1,16 +1,18 @@
-var menu = document.querySelector('.js-menu');
-var menuWidth = menu.offsetWidth; // get actual menu-block width
-menu.style.left = -(menuWidth + 1) + 'px';
-
-var showMenu = document.querySelector('.js-show-menu');
-var closeMenu = menu.querySelector('.js-close-menu');
-
-showMenu.onclick = function() {
-  menu.style.visibility = 'visible';
-  menu.style.left = '10px';
-};
-
-closeMenu.onclick = function() {
+addEventListener("load", function () {
+  var menu = document.querySelector('.js-menu');
+  var menuWidth = menu.offsetWidth; // get actual menu-block width
   menu.style.left = -(menuWidth + 1) + 'px';
-  menu.style.visibility = 'hidden';
-};
+
+  var showMenu = document.querySelector('.js-show-menu');
+  var closeMenu = menu.querySelector('.js-close-menu');
+
+  showMenu.onclick = function () {
+    menu.style.visibility = 'visible';
+    menu.style.left = '10px';
+  };
+
+  closeMenu.onclick = function () {
+    menu.style.left = -(menuWidth + 1) + 'px';
+    menu.style.visibility = 'hidden';
+  };
+});
